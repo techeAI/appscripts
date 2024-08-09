cb8c2f73730457611f58fc258ce87ba1aca0499e
mukeshk740 <mukeshk740@gmail.com>
2024-08-09 17:20:39 +0530

    focalboard

---
+++ focalboard/setup_focalboard.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+BASE_DIR=/mnt/DriveDATA
+apt install wget curl sudo -y 2> /dev/null
+if [ ! -x /usr/bin/docker ]; then
+echo "Installing docker.."
+sleep 2
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock 2> /dev/null
+else
+echo "Docker is already installed."
+sleep 2
+fi
+
+if sudo docker ps --format '{{.Names}}' | grep -q "focalboard"; then
+                                echo "The container 'focalboard' is already running. Skipping installation."
+                                sleep 2
+                        else
+                                echo "Setting up focalboard.."
+
+sudo docker run -dit --name focalboard --restart unless-stopped -v $BASE_DIR/focalboard:/opt/focalboard/data -p 5010:8000 mattermost/focalboard
+local_ip=$(ip route get 1 | awk '{print $7}')
+echo "#########################################################"
+echo "#########################################################"
+echo " "
+echo " "
+echo "login http://$local_ip:5010 to access focalboard."
+sleep 5
+fi
