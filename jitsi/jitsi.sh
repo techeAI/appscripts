e2cc0336781165086ad362b4df2470a3efff4822
mukeshk740 <mukeshk740@gmail.com>
2024-08-09 14:12:31 +0530

    updated Jitsi

--- jitsi/jitsi.sh
+++ jitsi/jitsi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt install sudo curl wget  unzip   -y 2> /dev/null
-
+BASE_DIR=/mnt/DriveDATA
 if [ ! -x /usr/bin/docker ]; then
 echo "Installing docker.."
 
@@ -17,18 +17,19 @@ fi
 sudo apt install docker-compose -y
 
 wget -qO JITSI.zip  $(curl -s https://api.github.com/repos/jitsi/docker-jitsi-meet/releases/latest | grep 'zip' | cut -d\" -f4)
-mkdir -p ./JITSI
-unzip JITSI.zip -d ./JITSI
-cd ./JITSI/*
+mkdir -p $BASE_DIR/JITSI
+unzip JITSI.zip -d $BASE_DIR/JITSI
+cd $BASE_DIR/JITSI/jitsi-docker-jitsi-me*
 cp env.example .env
 bash gen-passwords.sh
-read -p "Put the http port, you want to access Jisti On: " httpport
+httpport=5001
+httpsport=5002
 sed -i "s/8000/$httpport/g" .env
-read -p "Put the https port, you want to access Jisti On: " httpsport
 sed -i "s/8443/$httpsport/g" .env
-read -p "Enter the base directory path: " BASE_DIR
-sudo mkdir -p $BASE_DIR/.jitsi-meet-cfg/{web,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
-sed -i 's/8080/8090/g' docker-compose.yml 
+sed -i 's|CONFIG=~/.jitsi-meet-cfg|CONFIG=/mnt/DriveDATA/JITSI/.jitsi-meet-cfg|' .env
+sudo mkdir -p /mnt/DriveDATA/JITSI/.jitsi-meet-cfg/{web,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
+sed -i 's/8080/8090/g' docker-compose.yml
 docker-compose up -d
 local_ip=$(ip route get 1 | awk '{print $7}')
-echo "Jitsi running  on http://$local_ip:$httpport  and https://$local_ip:$httpsport " 
+echo "Jitsi running  on http://$local_ip:$httpport  and https://$local_ip:$httpsport "
+
