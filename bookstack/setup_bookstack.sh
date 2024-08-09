4b7e9f96196c65db74bb23bc3731df9b18c65168
mukeshk740 <mukeshk740@gmail.com>
2024-08-09 16:16:21 +0530

    bookstack

---
+++ bookstack/setup_bookstack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+apt install wget curl docker-compose sudo -y 2> /dev/null
+if [ ! -x /usr/bin/docker ]; then
+echo "Installing docker.."
+sleep 2
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock 2> /dev/null
+else
+echo "Docker is already installed."
+sleep 2
+fi
+
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/bookstack/docker-compose.yaml -o docker-compose.yaml
+
+docker-compose up -d
+local_ip=$(ip route get 1 | awk '{print $7}')
+echo "#########################################################"
+echo "#########################################################"
+echo " "
+echo " "
+echo "login http://$local_ip:3003 to access bookstack."
+sleep 5
