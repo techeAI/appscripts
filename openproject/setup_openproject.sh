0b93326984c57406646617df2ebc2a2c37eadcb1
mukeshk740 <mukeshk740@gmail.com>
2024-08-12 15:12:09 +0530

    openproject

---
+++ openproject/setup_openproject.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+BASE_DIR=/mnt/DriveDATA/openproject
+apt install git sudo curl wget  unzip   -y 2> /dev/null
+if [ ! -x /usr/bin/docker ]; then
+echo "Installing docker.."
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock 2> /dev/null
+else
+echo "Docker is already installed."
+sleep 2
+fi
+docker run --restart unless-stopped --publish 5004:5000 --detach --name whoogle-search benbusby/whoogle-search:latest
+
+docker run -d -p 9222:80 --name openproject --restart unless-stopped \
+  -e OPENPROJECT_HOST__NAME=openproject.openteche.io \
+  -e OPENPROJECT_SECRET_KEY_BASE=secret \
+  -v $BASE_DIR/pgdata:/var/openproject/pgdata \
+  -v $BASE_DIR/assets:/var/openproject/assets \
+  openproject/openproject:14
+  
+  
+local_ip=$(ip route get 1 | awk '{print $7}')
+echo "Now you can access ipscanner through URL: http://$local_ip:9222"
