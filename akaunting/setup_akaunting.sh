6df5e801e81787f12d36569e91fe45b8c7db871b
mukeshk740 <mukeshk740@gmail.com>
2024-08-13 14:14:48 +0530

    akaunting

---
+++ akaunting/setup_akaunting.sh
@@ -0,0 +1,18 @@
+if [ ! -x /usr/bin/docker ]; then
+echo "Installing docker.."
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock 2> /dev/null
+else
+echo "Docker is already installed."
+sleep 2
+fi
+ 
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/akaunting/docker-compose.yaml -o docker-compose.yaml
+docker-compose up -d
+local_ip=$(ip route get 1 | awk '{print $7}')
+echo "Now you can access Akaunting through URL: http://$local_ip:7070" 
