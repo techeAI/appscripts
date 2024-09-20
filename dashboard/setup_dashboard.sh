ccbd72a53b3dc9d0449a1a3144686a39e033426b
techeAI <git@teche.ai>
2024-09-20 14:43:18 +0530

    1234

--- dashboard/setup_dashboard.sh
+++ dashboard/setup_dashboard.sh
@@ -48,9 +48,9 @@ fi
 								sudo mkdir -p $BASE_DIR/teche-dashboard_icons
 								sudo mkdir -p $BASE_DIR/teche-dashboard_data
                                 sudo docker run  --name teche-dashboard  --restart unless-stopped  -p $hport:7575  -v $BASE_DIR/teche-dashboard_configs:/app/data/configs  -v /var/run/docker.sock:/var/run/docker.sock -v $BASE_DIR/teche-dashboard_icons:/app/public/icons -v $BASE_DIR/teche-dashboard_data:/data -d docker.io/techeai/techeos:latest
-#								curl -sL  https://raw.githubusercontent.com/techeAI/appscripts/main/dashboard/default.json -o default.json
+								curl -sL  https://raw.githubusercontent.com/techeAI/appscripts/main/dashboard/default.json -o default.json
 #								curl -sL  https://raw.githubusercontent.com/techeAI/appscripts/main/dashboard/public.json -o public.json
-#								mv default.json $BASE_DIR/teche-dashboard_configs/
+								mv default.json $BASE_DIR/teche-dashboard_configs/
 #								mv public.json $BASE_DIR/teche-dashboard_configs/
 
 wget -qO $BASE_DIR/teche-dashboard_icons/Biz-App.png https://openteche.s3.amazonaws.com/icons/Biz-App.png
