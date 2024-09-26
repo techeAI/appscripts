598b8e09f99b46354d12c25107f37abf9325e29c
techeAI <git@teche.ai>
2024-09-26 14:49:38 +0530

    email plugin

--- MantisBT/setup_bugtracker.sh
+++ MantisBT/setup_bugtracker.sh
@@ -20,8 +20,13 @@ mkdir -p $BASE_DIR/html
 mkdir -p $BASE_DIR/db
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/MantisBT/docker-compose.yaml -o docker-compose.yaml
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/MantisBT/techebt-nginx.conf -o techebt-nginx.conf
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/MantisBT/techebt-2.26.3.zip -o techebt-2.26.3.zip
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/MantisBT/EmailReporting_v0.10.1.zip -o EmailReporting_v0.10.1.zip
 mv techebt-nginx.conf /etc/nginx/sites-enabled/techebt
 docker-compose -f ./docker-compose.yaml up -d
+
+mv techebt-2.26.3.zip $BASE_DIR/html/
+cd $BASE_DIR/html/ && unzip techebt-2.26.3.zip
 
 chown -R www-data:www-data $BASE_DIR/html/
 local_ip=$(ip route get 1 | awk '{print $7}')
