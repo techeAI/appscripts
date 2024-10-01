6fab5947f7202d5922f26f1a72d94526ec3e3de9
TecheAI <git@teche.ai>
2024-10-01 16:39:46 +0530

    changess

--- HRMS/setup_hrms.sh
+++ HRMS/setup_hrms.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 apt install wget curl sudo -y 2> /dev/null
-BASE_DIR=/mnt/DriveDATA/mantis
-port_http=3001
+BASE_DIR=/mnt/DriveDATA/hrms
 if [ ! -x /usr/bin/docker ]; then
 echo "Installing docker.."
 sleep 2
@@ -16,17 +15,15 @@ echo "Docker is already installed."
 sleep 2
 fi
 apt install docker-compose -y
-mkdir -p $BASE_DIR/html
-mkdir -p $BASE_DIR/db
+mkdir -p $BASE_DIR
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/HRMS/hrms-nginx.conf -o hrms-nginx.conf
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/HRMS/HRMS.zip -o HRMS.zip
 mv hrms-nginx.conf /etc/nginx/sites-enabled/hrms
-docker-compose -f ./docker-compose.yaml up -d
-mv techebt-2.26.3.zip $BASE_DIR/html/
-cd $BASE_DIR/html/ && unzip techebt-2.26.3.zip
-chown -R www-data:www-data $BASE_DIR/html/
+unzip HRMS.zip
+cd HRMS && docker-compose -f ./docker-compose.yaml up -d
+chown -R www-data:www-data $BASE_DIR
 local_ip=$(ip route get 1 | awk '{print $7}')
-echo  "Please access the Bug Tracket at http://$local_ip:$port_http"
+echo  "Please access the HRMS at http://$local_ip:7072"
 echo ""
 echo ""
-echo "To Run Behind nginx proxy please set server_name in /etc/nginx/sites-enabled/techebt"
+echo "To Run Behind nginx proxy please set server_name in /etc/nginx/sites-enabled/hrms"
