07d461003d1d3325535c27f2ebf8dd192676c330
TecheAI <git@teche.ai>
2024-10-08 16:08:34 +0530

    bs

--- bookstack/setup_bookstack.sh
+++ bookstack/setup_bookstack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+read -p "Enter the URL: " app_url
 apt install wget curl docker-compose sudo -y 2> /dev/null
 if [ ! -x /usr/bin/docker ]; then
 echo "Installing docker.."
@@ -13,13 +14,17 @@ else
 echo "Docker is already installed."
 sleep 2
 fi
+mkdir -p /mnt/DriveDATA/bookstack/
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/bookstack/docker-compose.yaml -o docker-compose.yaml
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/bookstack/bs-nginx.conf -o bs-nginx.conf
+mv bs-nginx.conf /etc/nginx/sites-enabled/bs
 
-
-docker-compose up -d
+sed -i "s|ChangeMe-APP_URL|$app_url|g" ./docker-compose.yaml && docker-compose up -d
 local_ip=$(ip route get 1 | awk '{print $7}')
 echo "#########################################################"
 echo "#########################################################"
 echo " "
 echo " "
-echo "login http://$local_ip:3003 to access bookstack."
+echo "login http://$local_ip:7076 to access bookstack."
 sleep 5
+echo "To Run Behind nginx proxy please set server_name in /etc/nginx/sites-enabled/bs"
