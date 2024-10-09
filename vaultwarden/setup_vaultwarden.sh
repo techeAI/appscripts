a60b8d3c546230cba4b4db322adaff7416760031
TecheAI <git@teche.ai>
2024-10-09 12:13:22 +0530

    vw

--- vaultwarden/setup_vaultwarden.sh
+++ vaultwarden/setup_vaultwarden.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 apt install git sudo curl wget  unzip   -y 2> /dev/null
+BASE_DIR=/mnt/DriveDATA
+mkdir -p $BASE_DIR/vaultwarden
 if [ ! -x /usr/bin/docker ]; then
 echo "Installing docker.."
-
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo systemctl start docker
@@ -13,9 +14,10 @@ else
 echo "Docker is already installed."
 sleep 2
 fi
- 
-
-docker run -d --name vaultwarden --restart=unless-stopped -v $BASE_DIR/vaultwarden/:/data/ -p 5007:80 vaultwarden/server:latest
-
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/vaultwarden/vw-nginc.conf -o vw-nginc.conf
+mv vw-nginc.conf /etc/nginx/sites-enabled/vw
+docker run -dt --name vaultwarden --restart=unless-stopped -v $BASE_DIR/vaultwarden/:/data/ -p 7077:80 vaultwarden/server:1.32.1
 local_ip=$(ip route get 1 | awk '{print $7}')
-echo "Now you can access vaultwarden through URL: http://$local_ip:5007"
+echo "Now you can access vaultwarden through URL: http://$local_ip:7077"
+sleep 5
+echo "To Run Behind nginx proxy please set server_name in /etc/nginx/sites-enabled/vw"
