d6592924efa75309691ed2af0500f1ae98f31674
TecheAI <git@teche.ai>
2024-10-09 14:38:06 +0530

    fix

--- vaultwarden/setup_vaultwarden.sh
+++ vaultwarden/setup_vaultwarden.sh
@@ -16,20 +16,11 @@ sleep 2
 fi
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/vaultwarden/vw-nginc.conf -o vw-nginc.conf
 mv vw-nginc.conf /etc/nginx/sites-enabled/vw
-random_string=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 40)
 clear
 echo "Admin Panel Secret Key is : $random_string"
 echo "Please Note Down Key (It will not be recoverable)"
-read -p "If you have Noted Down the Key and want to continue ? (y/n)" answer
-answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
-if [[ "$answer" == "y" ]]; then
 docker run -dt --name vaultwarden --restart=unless-stopped -v $BASE_DIR/vaultwarden/:/data/ -p 7077:80 -e WEBSOCKET_ENABLED:true -e ADMIN_TOKEN:$random_string vaultwarden/server:1.32.1
 local_ip=$(ip route get 1 | awk '{print $7}')
 echo "Now you can access vaultwarden through URL: http://$local_ip:7077"
 sleep 5
 echo "To Run Behind nginx proxy please set server_name in /etc/nginx/sites-enabled/vw"
-elif [[ "$answer" == "n" ]]; then
-echo "You chose not to continue. Exiting.."
-else
-echo "Invalid response. Please enter y or n."
-fi
