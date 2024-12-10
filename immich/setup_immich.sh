dd95b6dbe38a4e83dcc006733f874f87c1019a44
TecheAI <git@teche.ai>
2024-12-10 12:05:31 +0530

    immich

--- immich/setup_immich.sh
+++ immich/setup_immich.sh
@@ -17,11 +17,13 @@ fi
 
 
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/immich/docker-compose.yml -o docker-compose.yml
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/immich/immich-nginx.conf -o immich-nginx.conf
+mv immich-nginx.conf /etc/nginx/sites-enabled/
 docker compose up -d
 local_ip=$(ip route get 1 | awk '{print $7}')
 echo "#########################################################"
 echo "#########################################################"
 echo " "
 echo " "
-echo "login http://$local_ip:5008 to access Immich."
+echo "login http://$local_ip:7086 to access Immich."
 sleep 5
