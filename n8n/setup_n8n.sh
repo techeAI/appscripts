7b0d9d4f60305cff256d01acbcd8849c43a2d56c
TecheAI <git@teche.ai>
2024-12-11 16:12:20 +0530

    fixed URL-n8n

--- n8n/setup_n8n.sh
+++ n8n/setup_n8n.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+read -p "Enter the URL(Do not add http or https): https://" app_url
 apt install wget curl docker-compose sudo -y 2> /dev/null
 if [ ! -x /usr/bin/docker ]; then
 echo "Installing docker.."
@@ -17,7 +18,7 @@ sudo mkdir /mnt/DriveDATA/n8n
 sudo chmod 777 /mnt/DriveDATA/n8n
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/n8n/n8n-nginx.conf -o n8n-nginx.conf
 mv n8n-nginx.conf /etc/nginx/sites-enabled/n8n
-docker run -dt  --name n8n  -p 7088:5678 -e GENERIC_TIMEZONE="Asia/Kolkata"  -e TZ="Asia/Kolkata"  -v /mnt/DriveDATA/n8n:/home/node/.n8n docker.n8n.io/n8nio/n8n:1.71.2
+docker run -dt  --name n8n  -p 7088:5678 -e GENERIC_TIMEZONE="Asia/Kolkata"  -e TZ="Asia/Kolkata" -e WEBHOOK_URL="https://n$app_url" -v /mnt/DriveDATA/n8n:/home/node/.n8n docker.n8n.io/n8nio/n8n:1.71.2
 local_ip=$(ip route get 1 | awk '{print $7}')
 echo "#########################################################"
 echo "#########################################################"
