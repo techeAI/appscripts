4dc53b687a37aa6e11e31f3509a98ba740b59ddd
TecheAI <git@teche.ai>
2025-01-29 17:31:44 +0530

    snipe-it

---
+++ snipe-it/setup_snipe.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+apt install git sudo curl wget  unzip   -y 2> /dev/null
+BASE_DIR=/mnt/DriveDATA/snipeit
+mkdir -p $BASE_DIR/db
+mkdir -p $BASE_DIR/storage
+#hex_key=$(openssl rand -hex 4)
+#echo "Generated key is: $hex_key"
+if [ ! -x /usr/bin/docker ]; then
+echo "Installing docker.."
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock 2> /dev/null
+else
+echo "Docker is already installed."
+sleep 2
+fi
+#read -p "Enter Full URL (without http or https) for docmost:- https://" url
+read -p "Do you want to set up SMTP? (yes/no): " response
+
+if [[ "$response" == "yes" || "$response" == "y" ]]; then
+read -p "Enter SMTP_HOST: " smtphost
+read -p "Enter SMTP_PORT: " smtpport
+read -p "Enter User Name: " smtpuname
+read -p "Enter FROM-Name: " smtpname
+read -p "Enter Passowrd: " smtppwd
+else
+  echo "Skipping SMTP setup."
+fi
+
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/snipe-it/snipe-nginx.conf -o snipe-nginx.conf
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/snipe-it/docker-compose.yaml -o docker-compose.yaml
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/snipe-it/snipe.env -o snipe.env
+#sed -i "s|ChangeMeAppURL|$url|g" docker-compose.yaml
+#sed -i "s|ChangeMeAppURL|$url|g" snipe.env
+sed -i "s|ChangeMeSMTPHOST|$smtphost|g" snipe.env
+sed -i "s|ChangeMePORT|$smtpport|g" snipe.env
+sed -i "s|ChangeMeUserNAME|$smtpuname|g" snipe.env
+sed -i "s|ChangeMeMailFrom|$smtpname|g" snipe.env
+sed -i "s|ChangeMePWD|$smtppwd|g" snipe.env
+mv snipe.env .env
+mv snipe-nginx.conf /etc/nginx/sites-enabled/snipe
+docker compose up -d
+sleep 5
+echo "To Run Behind nginx proxy please install SSL by certbot --nginx and open URL"
