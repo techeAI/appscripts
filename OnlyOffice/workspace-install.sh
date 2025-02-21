db398a38f96db03f8c0205ecffbccc26c0c3a2ae
TecheAI <git@teche.ai>
2025-02-21 16:06:24 +0530

    onlyoffice

--- OnlyOffice/workspace-install.sh
+++ OnlyOffice/workspace-install.sh
@@ -15,52 +15,13 @@ else
 				echo "Docker is already installed."
 				sleep 2
 fi				
-
-# (c) Copyright Ascensio System Limited 2010-2021
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
-# You can contact Ascensio System SIA by email at sales@onlyoffice.com
-
-# Function to read user input and update basedir in install.sh
-update_basedir() {
-    echo ""
-    echo ""
-    echo "Base directory is the path for directory in data disk , like /zfsDrive/zfsPool, if you don't have separate data disk, please put the path form OS disk like /etc/OT "
-    echo ""
-    read -p "Enter the Base directory path "" : " new_basedir
-    echo $new_basedir > /etc/basedir
+   new_basedir=/mnt/DriveDATA
 
     # Replace "changebasedir" in install.sh with the new path
     sed -i "s|changebasedir|$new_basedir|g" ./install.sh
 
     echo "Base directory updated successfully in install.sh."
-}
 
-# Check if /etc/basedir file exists
-if [ -e "/etc/basedir" ]; then
-    # Read the current basedir from the file
-    current_basedir=$(cat "/etc/basedir")
-
-    # Ask the user if they want to proceed with the current basedir
-    read -p "Base directory found in /etc/basedir: $current_basedir. Do you want to proceed with this? (y/n): " choice
-
-
-    if [ "$choice" = "y" ]; then
-        # User does not want to proceed with the current basedir, update it
-     sed -i "s|changebasedir|$current_basedir|g" ./install.sh
-        else
-        # User does not want to proceed with the current basedir, update it
-        update_basedir
-    fi
-else
-update_basedir
- fi
 
 set -e
 
