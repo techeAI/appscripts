bcb54cac6fb7a97598e4c2f2b9a75931ee3831fd
TecheAI <git@teche.ai>
2025-03-10 17:50:08 +0530

    clamav

--- antivirus/antivirus-nginx-conf.sh
+++ antivirus/antivirus-nginx-conf.sh
@@ -3,4 +3,9 @@ server {
     server_name antivirus.domainname;  # Replace with your domain or IP
 
     root /var/www/html/scans;  # Directory where your HTML files are stored
+        location / {
+        autoindex on;
+        autoindex_exact_size off;
+        autoindex_localtime on;
+    }
 }
