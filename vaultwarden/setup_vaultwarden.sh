c766727285eb43252676567c18d2485ab805e0a6
TecheAI <git@teche.ai>
2025-04-14 12:38:00 +0530

    vw

--- vaultwarden/setup_vaultwarden.sh
+++ vaultwarden/setup_vaultwarden.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 apt install git sudo curl wget  unzip   -y 2> /dev/null
 BASE_DIR=/mnt/DriveDATA
-mkdir -p $BASE_DIR/vaultwarden
+mkdir -p $BASE_DIR/vaultwarden/ssl
+local_ip=$(ip route get 1 | awk '{print $7}')
 if [ ! -x /usr/bin/docker ]; then
 echo "Installing docker.."
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -16,8 +17,20 @@ sleep 2
 fi
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/vaultwarden/vw-nginc.conf -o vw-nginc.conf
 mv vw-nginc.conf /etc/nginx/sites-enabled/vw
+
+read -p "Will this deployment be publicly accessible? (yes/no): " PUBLIC_DEPLOY
+if [[ "$PUBLIC_DEPLOY" == "yes" ]]; then
+echo "Setting up for public deployment..."
 docker run -dt --name vaultwarden --restart=unless-stopped -v $BASE_DIR/vaultwarden/:/data/ -p 7077:80 vaultwarden/server:1.32.1
-local_ip=$(ip route get 1 | awk '{print $7}')
-echo "Now you can access vaultwarden through URL: http://$local_ip:7077"
+urlscheme="http"
+elif [[ "$PUBLIC_DEPLOY" == "no" ]]; then
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout $BASE_DIR/vaultwarden/ssl/vw.key -out $BASE_DIR/vaultwarden/ssl/vw.crt -subj "/C=CT/ST=State/L=City/O=MyOrg/OU=Unit/CN=$local_ip"
+docker run -dt --name vaultwarden --restart=unless-stopped -v /mnt/DriveDATA/vaultwarden/:/data/ -e ROCKET_TLS='{certs="/data/ssl/vw.crt",key="/data/ssl/vw.key"}' -p 7077:80 vaultwarden/server:1.32.1
+urlscheme="https"
+else
+    echo "Invalid response. Please enter 'yes' or 'no'."
+    exit 1
+fi
+echo "Now you can access vaultwarden through URL: $urlscheme://$local_ip:7077"
 sleep 5
 echo "To Run Behind nginx proxy please set server_name in /etc/nginx/sites-enabled/vw"
