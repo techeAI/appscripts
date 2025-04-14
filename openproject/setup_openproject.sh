3f3a93cb686a6f6971a97e47b03d29104f2c595d
TecheAI <git@teche.ai>
2025-04-14 14:30:00 +0530

    op

--- openproject/setup_openproject.sh
+++ openproject/setup_openproject.sh
@@ -3,8 +3,6 @@ BASE_DIR=/mnt/DriveDATA/openproject
 PORT=7079
 random_key=$(openssl rand -hex 16)
 local_ip=$(ip route get 1 | awk '{print $7}')
-
-
 apt install git sudo curl wget  unzip   -y 2> /dev/null
 if [ ! -x /usr/bin/docker ]; then
 echo "Installing docker.."
@@ -20,8 +18,6 @@ sleep 2
 fi
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/openproject/openproject-nginx.conf -o openproject-nginx.conf
 sleep 2
-sed -i "s|prefixproject.domainname|$hname|g" ./openproject-nginx.conf
-mv openproject-nginx.conf /etc/nginx/sites-enabled/openproject
 sudo mkdir -p $BASE_DIR/pgdata 
 sudo mkdir -p $BASE_DIR/assets
 
@@ -29,6 +25,8 @@ read -p "Will this deployment be publicly accessible? (yes/no): " PUBLIC_DEPLOY
 if [[ "$PUBLIC_DEPLOY" == "yes" ]]; then
 echo "Setting up for public deployment..."
 read -p "Set hostname (without http or https) for openproject : " hname
+sed -i "s|prefixproject.domainname|$hname|g" ./openproject-nginx.conf
+mv openproject-nginx.conf /etc/nginx/sites-enabled/openproject
 docker run -dt -p $PORT:80 --name openproject --restart unless-stopped \
   -e OPENPROJECT_HOST_NAME=$hname \
   -e OPENPROJECT_SECRET_KEY_BASE=$random_key \
