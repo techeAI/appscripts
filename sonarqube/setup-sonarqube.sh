a3b4f1507e44387ac8c12b5954bf2bf6a28646ad
TecheAI <git@teche.ai>
2025-04-17 18:17:16 +0530

    sonar

--- sonarqube/setup-sonarqube.sh
+++ sonarqube/setup-sonarqube.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 #read -p "Enter the URL(Do not add http or https): https://" app_url
+echo "Backup /etc/sysctl.conf"
+sudo cp /etc/sysctl.conf /etc/sysctl.conf.bak-orig.$(date +%F-%H%M%S)
+echo "Backup created: /etc/sysctl.conf.bak-orig.$(date +%F-%H%M%S)"
+
+# Check and append if needed
+VM_SETTING="vm.max_map_count=262144"
+FS_SETTING="fs.file-max=65536"
+
+# Append only if not already set
+grep -q "^$VM_SETTING" /etc/sysctl.conf || echo "$VM_SETTING" | sudo tee -a /etc/sysctl.conf
+grep -q "^$FS_SETTING" /etc/sysctl.conf || echo "$FS_SETTING" | sudo tee -a /etc/sysctl.conf
+
+# Apply changes
+sudo sysctl -p
+
 BASE_DIR=/mnt/DriveDATA/
 mkdir -p $BASE_DIR/sonarqube/{sonarqube_conf,sonarqube_data,sonarqube_extensions,sonarqube_logs,sonarqube_temp,sonar_db,sonar_db_data}
 
