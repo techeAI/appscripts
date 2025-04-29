4d728602ac38e668ff366f83e825e6a6f8c315ed
TecheAI <git@teche.ai>
2025-04-29 18:20:24 +0530

    erpnext

---
+++ erpnext/setup-erpnext.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+apt install git sudo curl wget  unzip   -y 2> /dev/null
+BASE_DIR=/mnt/DriveDATA/erpnext
+mkdir -p $BASE_DIR/erpnext/{mariadb,sites}
+if [ ! -x /usr/bin/docker ]; then
+echo "Installing docker.."
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock 2> /dev/null
+else
+echo "Docker is already installed."
+sleep 2
+fi
+read -p "Enter URL to access on browser (without http/https)(ex. erpnext.teche.ai): " url
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/erpnext/erpnext-nginx.conf -o erpnext-nginx.conf
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/erpnext/docker-compose.yaml -o docker-compose.yaml
+sed -i "s|changeme-url|$url|g" docker-compose.yaml
+sed -i "s|Changeme-url|$url|g" erpnext-nginx.conf
+mv erpnext-nginx.conf /etc/nginx/sites-available/erpnext
+ln -s /etc/nginx/sites-available/erpnext /etc/nginx/sites-enabled/erpnext
+docker compose up -d
+#local_ip=$(ip route get 1 | awk '{print $7}')
+#echo "Now you can access tunnel through URL: https://$url"
+#sleep 5
+echo "To Run Behind nginx proxy please set server_name in /etc/nginx/sites-enabled/erpnext"
