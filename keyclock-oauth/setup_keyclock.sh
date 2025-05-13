2400f24d8e2cbdb3efb2155b34a16d0d0e36113a
TecheAI <git@teche.ai>
2025-05-13 12:46:42 +0530

    keyclock

---
+++ keyclock-oauth/setup_keyclock.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+apt install git sudo curl wget  unzip   -y 2> /dev/null
+BASE_DIR=/mnt/DriveDATA/
+mkdir -p $BASE_DIR/{postgres,keycloak}
+sudo chown -R 1000:1000 $BASE_DIR/keycloak
+if [ ! -x /usr/bin/docker ]; then
+echo "Installing docker.."
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+sudo setfacl --modify user:$USER:rw /var/run/docker.sock 2> /dev/null
+else
+echo "Docker is already installed."
+sleep 2
+fi
+read -p "Enter URL to access keyclock on browser (without http/https)(ex. auth.teche.ai): " url
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/keyclock-oauth/keyclock-nginx.conf -o keyclock-nginx.conf
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/keyclock-oauth/docker-compose.yaml -o docker-compose.yaml
+sed -i "s|changeme-url|$url|g" docker-compose.yaml
+sed -i "s|changeme-url|$url|g" keyclock-nginx.conf
+mv keyclock-nginx.conf /etc/nginx/sites-available/keyclock
+ln -s /etc/nginx/sites-available/keyclock /etc/nginx/sites-enabled/keyclock
+docker compose up -d
+echo "install SSL certificate an open URL in browser: https://$url"
