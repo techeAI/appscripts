cb613376c120cdd952f356126a4a1f9b470d50a3
TecheAI <git@teche.ai>
2025-06-03 18:35:14 +0530

    fgfdgfdg

--- nginx-ui/setup-nginxui.sh
+++ nginx-ui/setup-nginxui.sh
@@ -5,6 +5,27 @@ sed -i "s|prefixwebsrv.domainname|$url|g" ./websrv-nginx.conf
 mv websrv-nginx.conf /etc/nginx/sites-available/websrv
 ln -s /etc/nginx/sites-available/websrv /etc/nginx/sites-enabled/websrv
 bash <(curl -L -s https://raw.githubusercontent.com/0xJacky/nginx-ui/master/install.sh) install
+sleep 5
+INI_FILE="/usr/local/etc/nginx-ui/app.ini"
+
+# Comment out any existing lines for AccessLogPath and ErrorLogPath
+sed -i '/^\[nginx\]/,/^\[/{s/^\(AccessLogPath[ \t]*=\)/# \1/; s/^\(ErrorLogPath[ \t]*=\)/# \1/}' "$INI_FILE"
+
+# Append new lines just after [nginx] section
+awk -v access="/var/log/nginx/access.log" -v error="/var/log/nginx/error.log" '
+    BEGIN { inserted=0 }
+    /^\[nginx\]/ {
+        print
+        if (!inserted) {
+            print "AccessLogPath   = " access
+            print "ErrorLogPath    = " error
+            inserted=1
+        }
+        next
+    }
+    { print }
+' "$INI_FILE" > "${INI_FILE}.tmp" && mv "${INI_FILE}.tmp" "$INI_FILE"
+
 
 #http://your-server-ip:9000
 #
