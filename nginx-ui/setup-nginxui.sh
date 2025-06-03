a5a351dd7c23df278e592a881ee87855fa168ca4
TecheAI <git@teche.ai>
2025-06-03 18:03:32 +0530

    dsfdsfdsf

--- nginx-ui/setup-nginxui.sh
+++ nginx-ui/setup-nginxui.sh
@@ -1,7 +1,10 @@
 apt install nginx nginx-common -y
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/nginx-ui/websrv-nginx.conf -o websrv-nginx.conf
+url=$(grep "^nginxui_url=" /mnt/DriveDATA/Deploy-config/urls.conf | cut -d'=' -f2)
+sed -i "s|prefixwebsrv.domainname|$url|g" ./websrv-nginx.conf
+mv websrv-nginx.conf /etc/nginx/sites-available/websrv
+ln -s /etc/nginx/sites-available/websrv /etc/nginx/sites-enabled/websrv
 bash <(curl -L -s https://raw.githubusercontent.com/0xJacky/nginx-ui/master/install.sh) install
-
-
 
 #http://your-server-ip:9000
 #
