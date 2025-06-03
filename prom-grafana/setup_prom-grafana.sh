8a55c1b350212950a91678c7261a9db0eec79e80
TecheAI <git@teche.ai>
2025-06-03 13:12:00 +0530

    dfvvdfd

--- prom-grafana/setup_prom-grafana.sh
+++ prom-grafana/setup_prom-grafana.sh
@@ -1,6 +1,9 @@
 
 #!/bin/bash
 apt install git sudo curl wget  unzip   -y 2> /dev/null
+#read -p "Enter URL to access Grafana on browser (without http/https)(ex. grafana.teche.ai): " url
+url=$(grep "^grafana_url=" /mnt/DriveDATA/Deploy-config/urls.conf | cut -d'=' -f2)
+promurl=$(grep "^prometheus_url=" /mnt/DriveDATA/Deploy-config/urls.conf | cut -d'=' -f2)
 BASE_DIR=/mnt/DriveDATA/
 mkdir -p $BASE_DIR/{grafana,grafana/alerting,prometheus}
 if [ ! -x /usr/bin/docker ]; then
@@ -15,18 +18,21 @@ else
 echo "Docker is already installed."
 sleep 2
 fi
-read -p "Enter URL to access Grafana on browser (without http/https)(ex. grafana.teche.ai): " url
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/prom-grafana/prom-nginx.conf -o prom-nginx.conf
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/prom-grafana/grafana-nginx.conf -o grafana-nginx.conf
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/prom-grafana/prometheus.yml -o prometheus.yml
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/prom-grafana/docker-compose.yaml -o docker-compose.yaml
 sed -i "s|changeme-url|$url|g" docker-compose.yaml
 sed -i "s|changeme-url|$url|g" grafana-nginx.conf
+sed -i "s|changeme-url|$promurl|g" prom-nginx.conf
 mv prom-nginx.conf /etc/nginx/sites-available/prom
 mv grafana-nginx.conf /etc/nginx/sites-available/grafana
+ln -s /etc/nginx/sites-available/prom /etc/nginx/sites-enabled/prom
+ln -s /etc/nginx/sites-available/grafana /etc/nginx/sites-enabled/grafana
 mv prometheus.yml /mnt/DriveDATA/prometheus/prometheus.yml
+
 sudo chown -R 472:472 /mnt/DriveDATA/grafana  # Grafana UID
 sudo chown -R 65534:65534 /mnt/DriveDATA/prometheus
-ln -s /etc/nginx/sites-available/grafana /etc/nginx/sites-enabled/grafana
+
 docker compose up -d
 echo "To Run Behind nginx proxy please set server_name in /etc/nginx/sites-enabled/grafana"
