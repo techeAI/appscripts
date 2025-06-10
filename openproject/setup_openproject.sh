4245cbcaa8215b261ac49cb0b6b8c7200818d7a8
TecheAI <git@teche.ai>
2025-06-10 10:21:03 +0530

    op

--- openproject/setup_openproject.sh
+++ openproject/setup_openproject.sh
@@ -3,6 +3,10 @@ BASE_DIR=/mnt/DriveDATA/openproject
 PORT=7079
 random_key=$(openssl rand -hex 16)
 local_ip=$(ip route get 1 | awk '{print $7}')
+#read -p "Will this deployment be publicly accessible? (yes/no): " PUBLIC_DEPLOY
+PUBLIC_DEPLOY=$(grep "^openproject_public_deploy=" /mnt/DriveDATA/Deploy-config/urls.conf | cut -d'=' -f2)
+#read -p "Set hostname (without http or https) for openproject : " hname
+hname=$(grep "^openproject_url=" /mnt/DriveDATA/Deploy-config/urls.conf | cut -d'=' -f2)
 apt install git sudo curl wget  unzip   -y 2> /dev/null
 if [ ! -x /usr/bin/docker ]; then
 echo "Installing docker.."
@@ -20,13 +24,11 @@ curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/openproject/o
 sleep 2
 sudo mkdir -p $BASE_DIR/pgdata 
 sudo mkdir -p $BASE_DIR/assets
-
-read -p "Will this deployment be publicly accessible? (yes/no): " PUBLIC_DEPLOY
 if [[ "$PUBLIC_DEPLOY" == "yes" ]]; then
 echo "Setting up for public deployment..."
-read -p "Set hostname (without http or https) for openproject : " hname
 sed -i "s|prefixproject.domainname|$hname|g" ./openproject-nginx.conf
-mv openproject-nginx.conf /etc/nginx/sites-enabled/openproject
+mv openproject-nginx.conf /etc/nginx/sites-available/openproject
+ln -s /etc/nginx/sites-available/openproject /etc/nginx/sites-enabled/openproject
 docker run -dt -p $PORT:80 --name openproject --restart unless-stopped \
   -e OPENPROJECT_HOST_NAME=$hname \
   -e OPENPROJECT_SECRET_KEY_BASE=$random_key \
