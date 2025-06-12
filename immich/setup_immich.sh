28386be73e6460cf5893d804e57ed1890ee54f42
TecheAI <git@teche.ai>
2025-06-12 13:32:36 +0530

    dfdfdd

--- immich/setup_immich.sh
+++ immich/setup_immich.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apt install wget curl docker-compose sudo -y 2> /dev/null
+url=$(grep "^immich_url=" /mnt/DriveDATA/Deploy-config/urls.conf | cut -d'=' -f2)
 mkdir -p /mnt/DriveDATA/immich/immich_server
 mkdir -p /mnt/DriveDATA/immich/immich_server_db
 if [ ! -x /usr/bin/docker ]; then
@@ -21,7 +22,9 @@ fi
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/immich/docker-compose.yml -o docker-compose.yml
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/immich/immich-nginx.conf -o immich-nginx.conf
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/immich/.env -o .env
-mv immich-nginx.conf /etc/nginx/sites-enabled/
+sed -i "s|prefixphoto.domainname|$url|g" ./immich-nginx.conf
+mv immich-nginx.conf /etc/nginx/sites-available/immich
+ln -s /etc/nginx/sites-available/immich /etc/nginx/sites-enabled/immich
 docker compose up -d
 local_ip=$(ip route get 1 | awk '{print $7}')
 echo "#########################################################"
