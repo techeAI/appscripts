bbb786f093c3dc0aff7a7319ada6eda26befbc7a
TecheAI <git@teche.ai>
2025-08-01 11:19:07 +0530

    zabbix agent

--- Terraform/AWS-Cloud/deploy-maestroapp.sh
+++ Terraform/AWS-Cloud/deploy-maestroapp.sh
@@ -1,6 +1,6 @@
 ## Install common packages
 sudo apt update -y
-apt install htop curl snapd -y
+apt install htop curl snapd zabbix-agent -y
 apt-get remove certbot
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
