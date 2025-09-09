e20a0d58175f2ef9520746dadb5a39cb5f73e9c0
TecheAI <git@teche.ai>
2025-09-09 21:22:51 +0530

    loki + promtail

--- prom-grafana/setup_prom-grafana.sh
+++ prom-grafana/setup_prom-grafana.sh
@@ -5,7 +5,8 @@ apt install git sudo curl wget  unzip   -y 2> /dev/null
 url=$(grep "^grafana_url=" /mnt/DriveDATA/Deploy-config/urls.conf | cut -d'=' -f2)
 promurl=$(grep "^prometheus_url=" /mnt/DriveDATA/Deploy-config/urls.conf | cut -d'=' -f2)
 BASE_DIR=/mnt/DriveDATA/
-mkdir -p $BASE_DIR/{grafana,grafana/alerting,prometheus}
+mkdir -p $BASE_DIR/{grafana,grafana/alerting,prometheus,loki,promtail}
+chown -R 10001:10001 $BASE_DIR/loki
 if [ ! -x /usr/bin/docker ]; then
 echo "Installing docker.."
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -22,11 +23,17 @@ curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/prom-grafana/
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/prom-grafana/grafana-nginx.conf -o grafana-nginx.conf
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/prom-grafana/prometheus.yml -o prometheus.yml
 curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/prom-grafana/docker-compose.yaml -o docker-compose.yaml
+
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/prom-grafana/promtail-config.yml -o promtail-config.yml
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/prom-grafana/loki-config.yml -o loki-config.yml
+
 sed -i "s|changeme-url|$url|g" docker-compose.yaml
 sed -i "s|changeme-url|$url|g" grafana-nginx.conf
 sed -i "s|changeme-url|$promurl|g" prom-nginx.conf
 mv prom-nginx.conf /etc/nginx/sites-available/prom
 mv grafana-nginx.conf /etc/nginx/sites-available/grafana
+mv promtail-config.yml $BASE_DIR/promtail/promtail-config.yml
+mv loki-config.yml $BASE_DIR/loki/loki-config.yml
 #ln -s /etc/nginx/sites-available/prom /etc/nginx/sites-enabled/prom
 ln -s /etc/nginx/sites-available/grafana /etc/nginx/sites-enabled/grafana
 mv prometheus.yml /mnt/DriveDATA/prometheus/prometheus.yml
