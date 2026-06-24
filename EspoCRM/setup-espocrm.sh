e82ea1efbb39c2976266090f31c2502c4bb3544f
TecheAI <git@teche.ai>
2026-06-24 14:34:08 +0530

    espo

--- EspoCRM/setup-espocrm.sh
+++ EspoCRM/setup-espocrm.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 url=$(grep "^espo_crm=" /mnt/DriveDATA/Deploy-config/urls.conf | cut -d'=' -f2)
 apt install git sudo curl wget  unzip   -y 2> /dev/null
-BASE_DIR=/mnt/DriveDATA/odoo
-mkdir -p $BASE_DIR/{odoo-web-data,config,addons,odoo-db-data}
-chown -R 101:101 $BASE_DIR
+BASE_DIR=/mnt/DriveDATA/espocrm
+mkdir -p $BASE_DIR/{db,data}
 if [ ! -x /usr/bin/docker ]; then
 echo "Installing docker.."
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -17,13 +16,13 @@ echo "Docker is already installed."
 sleep 2
 fi
 #read -p "Enter URL to access on browser (without http/https)(ex. erpnext.teche.ai): " url
-curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/odoo/odoo-nginx.conf -o odoo-nginx.conf
-curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/odoo/docker-compose.yaml -o docker-compose.yaml
-sed -i "s|changemeurl|$url|g" ./odoo-nginx.conf
-mv odoo-nginx.conf /etc/nginx/sites-available/odoo
-ln -s /etc/nginx/sites-available/odoo /etc/nginx/sites-enabled/odoo
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/EspoCRM/espocrm-nginx.conf -o espocrm-nginx.conf
+curl -sL https://raw.githubusercontent.com/techeAI/appscripts/main/EspoCRM/docker-compose.yaml -o docker-compose.yaml
+sed -i "s|changemeurl|$url|g" ./espocrm-nginx.conf
+mv espocrm-nginx.conf /etc/nginx/sites-available/espocrm
+ln -s /etc/nginx/sites-available/espocrm /etc/nginx/sites-enabled/espocrm
 docker compose up -d
 #local_ip=$(ip route get 1 | awk '{print $7}')
 #echo "Now you can access tunnel through URL: https://$url"
 #sleep 5
-echo "To Run Behind nginx proxy please set server_name in /etc/nginx/sites-enabled/odoo"
+echo "To Run Behind nginx proxy please set server_name in /etc/nginx/sites-enabled/espocrm"
